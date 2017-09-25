f7ab7f4de816f5aae46b91e8a20a7d92f6231b6a
Patrick Cornelißen <cornelis@pcornelissen.de>
2017-09-25 07:59:39 +0200

    Use === instead of == to compare numbers

--- bootstrap-number-input.js
+++ bootstrap-number-input.js
@@ -67,7 +67,7 @@
 			// remove spins from original
 			clone.prop('type', 'text').keydown(function(e) {
 				if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
-					(e.keyCode == 65 && e.ctrlKey === true) ||
+					(e.keyCode === 65 && e.ctrlKey === true) ||
 					(e.keyCode >= 35 && e.keyCode <= 39)) {
 					return;
 				}
